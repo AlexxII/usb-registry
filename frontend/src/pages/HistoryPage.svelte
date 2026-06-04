1e85a761bb4315f0a36537c210b8a6dd77b85746
AlexxII_work <murmus51@yandex.ru>
2026-06-04 17:57:31 +0300

    history updated

--- frontend/src/pages/HistoryPage.svelte
+++ frontend/src/pages/HistoryPage.svelte
@@ -8,28 +8,49 @@
     <table class="table table-zebra">
       <thead>
         <tr>
+          <th>№ п.п.</th>
           <th>Производитель</th>
+          <th>Гриф</th>
           <th>Ответственный</th>
           <th>Рег №</th>
           <th>s/n</th>
           <th>Предписание</th>
-          <th>Зоны 2</th>
-          <th>Назначение</th>
+          <th>Зона 2</th>
+          <th>СД</th>
           <th>Прочее</th>
         </tr>
       </thead>
 
       <tbody>
-        {#each usbDevices as usb}
+        {#each usbDevices as usb, index}
           <tr>
-          <td>{usb.manufacturer}</td>
-          <td>{usb.owner}</td>
-          <td>{usb.registerNumber}</td>
-          <td>{usb.serial}</td>
-          <td>{usb.prescription}</td>
-          <td>{usb.zones}</td>
-          <td>{usb.capacity}</td>
-          <td>{usb.secclass}</td>
+            <td>{index + 1}</td>
+            <td>{usb.manufacturer}</td>
+            <td>
+              {#if usb.registered && !usb.secret}
+                <div class="tooltip" data-tip="Для ИНТЕРНЕТ">
+                  <div class="badge badge-sm bg-red-700">И</div>
+                </div>
+              {:else}
+                {usb.secclass ?? "-"}
+              {/if}
+            </td>
+            <td>{usb.owner ?? "-"}</td>
+            <td>{usb.registerNumber ?? "-"}</td>
+            <td>{usb.serial ?? "-"}</td>
+            <td>{usb.prescription ?? "-"}</td>
+            <td>{usb.zones ?? "-"}</td>
+            <td>
+              {#if usb.registered && usb.special}
+                <div
+                  aria-label="status"
+                  class="status status-xl bg-red-700"
+                ></div>
+              {:else}
+                -
+              {/if}
+            </td>
+            <td> <button class="btn btn-ghost btn-xs">Подробнее</button></td>
           </tr>
         {/each}
       </tbody>
